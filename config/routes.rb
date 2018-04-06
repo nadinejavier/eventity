5d381e10d18198e557fb1612c0c568fea9ae2555
Nadine <nad4ne@gmail.com>
2018-04-06 15:39:49 -0700

    deployed to heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   post 'user_token' => 'user_token#create'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'events#index'
   resources :users
   resources :events
 end
