6da0559ae02b9d126065de4f05ecd6976d4ff7c4
Nadine <nad4ne@gmail.com>
2018-04-23 18:31:34 -0700

    user event create is not working

--- app/controllers/user_events_controller.rb
+++ app/controllers/user_events_controller.rb
@@ -1,12 +1,26 @@
 class UserEventsController < ApplicationController
-    def create 
+  #   def create 
+  #   user_event = UserEvent.new(
+  #     user_id: params[:user_id],
+  #     event_id: params[:event_id])
+  #   if user_event.save
+  #     render json: {message: "Successfully joined event!"} 
+  #   else
+  #     render json: {errors: user_event.errors.full_messages}, status: :bad_request
+  #   end
+  # end 
+
+
+  def create
     user_event = UserEvent.new(
-      user_id: current_user.id,
-      event_id: params[:event_id])
+      event_id: params["event_id"],
+      user_id: params["user_id"]
+    )
+
     if user_event.save
-      render json: {message: "Successfully joined event!"} 
+      render json: {message: "You are now attending this event!"}
     else
-      render json: {errors: user_event.errors.full_messages}, status: :bad_request
+      render json: {errors: user_event.errors.full_messages}, status: :unprocessable_entity
     end
-  end 
+  end
 end
